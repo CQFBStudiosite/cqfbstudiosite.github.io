1fa92d9491c7ed967a7d3a111f7eb21eb601bea1
CQFBStudiosite <148546884+CQFBStudiosite@users.noreply.github.com>
2024-03-03 20:19:30 +0800

    upload ./js/script.js
    init the new site

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function to_download() {
+    window.open("./download.html");
+}
+function to_tutorial() {
+    window.open("./tutorial.html");
+}
+function download_lmc() {
+    window.open("https://wwk.lanzouj.com/im0Y71oy7nxi")
+}
